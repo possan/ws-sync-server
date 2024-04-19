c723b68a507e87c2becfec43a132c2520c1ecd28
Per-Olov Jernberg <possan@possan.se>
2024-04-20 01:34:12 +0200

    Add stats broadcast

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import * as uuid from "uuid";
 
 const PORT = process.env.PORT || 3000;
-
+const channelmetrics = {};
 const app = express();
 const appWithWS = expressWs(app);
 
@@ -17,53 +17,108 @@ function broadcast(message, channel) {
   // console.log("broadcast", channel, message);
   wss.clients.forEach(function (client) {
     // console.log("  client @channel ", client._channel);
-    if (client._channel === channel) {
+    if (client._channel === channel || channel === undefined) {
       client.send(message);
     }
   });
 }
 
-app.ws("/broadcast", function (ws, req) {
-  let nodeid = -1;
-  let channel = "default";
-  const id = uuid.v4();
+setInterval(() => {
+  // broadcast some stats
+  let channels = [];
+  let counter = 0;
+  let channelcounter = {};
 
-  console.log("connected", channel, id);
+  wss.clients.forEach(function (client) {
+    const channel = client._channel;
 
-  ws._channel = channel;
-
-  ws.send(
-    JSON.stringify({
-      type: "_welcome",
-      _channel: channel,
-      _id: id,
-    })
-  );
-
-  ws.on("message", function (msg) {
-    // console.log("got message", msg);
-    const decoded = JSON.parse(msg);
-    if (decoded && decoded.type === ".hello" && decoded.node) {
-      nodeid = decoded.node;
+    if (channels.indexOf(channel) === -1) {
+      channels.push(channel);
     }
-    decoded._channel = channel;
-    decoded._id = id;
-    broadcast(JSON.stringify(decoded), channel);
+
+    counter++;
+
+    if (channelcounter[channel] !== undefined) {
+      channelcounter[channel] += 1;
+    } else {
+      channelcounter[channel] = 1;
+    }
   });
 
-  ws.on("close", () => {
-    console.log("disconnect", channel, id, nodeid);
-    broadcast(
-      JSON.stringify({
-        type: ".disconnect",
-        _node: nodeid,
-        _channel: channel,
-        _id: id,
-      }),
-      channel
-    );
+  // console.log("channels", channels, counter);
+  channels = channels.map((c) => {
+    return {
+      id: c,
+      metrics: channelmetrics[c],
+      connections: channelcounter[c] || 0,
+    };
   });
-});
+
+  broadcast(
+    JSON.stringify({
+      type: ".stats",
+      clients: counter,
+      channels,
+    }),
+    undefined
+  );
+}, 3000);
+
+// app.ws("/broadcast", function (ws, req) {
+//   let nodeid = -1;
+//   let channel = "default";
+//   const id = uuid.v4();
+
+//   console.log("connected", channel, id);
+
+//   ws._channel = channel;
+
+//   ws.send(
+//     JSON.stringify({
+//       type: "_welcome",
+//       _channel: channel,
+//       _id: id,
+//     })
+//   );
+
+//   ws.on("message", function (msg) {
+//     // console.log("got message", msg);
+//     const decoded = JSON.parse(msg);
+//     if (decoded && decoded.type === ".hello" && decoded.node) {
+//       nodeid = decoded.node;
+//     }
+//     decoded._channel = channel;
+//     decoded._id = id;
+
+//     if (channel) {
+//       broadcast(JSON.stringify(decoded), channel);
+
+//       let metric = channelmetrics[channel];
+//       if (!metric) {
+//         metric = {};
+//         metric.lastseen = Date.now();
+//         metric.events = 1;
+//         channelmetrics[channel] = metric;
+//       } else {
+//         metric.lastseen = Date.now();
+//         metric.events++;
+//       }
+//     }
+//   });
+
+//   ws.on("close", () => {
+//     console.log("disconnect", channel, id, nodeid);
+//     broadcast(
+//       JSON.stringify({
+//         type: ".disconnect",
+//         _node: nodeid,
+//         _channel: channel,
+//         _id: id,
+//       }),
+//       channel
+//     );
+//   });
+// });
 
 app.ws("/broadcast/:channel", function (ws, req) {
   let nodeid = -1;
@@ -90,7 +145,21 @@ app.ws("/broadcast/:channel", function (ws, req) {
     }
     decoded._channel = channel;
     decoded._id = id;
-    broadcast(JSON.stringify(decoded), channel);
+
+    if (channel) {
+      broadcast(JSON.stringify(decoded), channel);
+
+      let metric = channelmetrics[channel];
+      if (!metric) {
+        metric = {};
+        metric.lastseen = Date.now();
+        metric.events = 1;
+        channelmetrics[channel] = metric;
+      } else {
+        metric.lastseen = Date.now();
+        metric.events++;
+      }
+    }
   });
 
   ws.on("close", () => {
